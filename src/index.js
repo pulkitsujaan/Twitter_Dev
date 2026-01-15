49b6fb2f1ca1086e419e3f70a08ade9c482c3f6c
Pulkit Sujaan <pulkitsujaan2708@gmail.com>
2026-01-15 13:01:22 +0530

    added v1 for tweet model and hashtag model

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 const express = require('express');
 const connect = require('./config/databases')
 
-const TweetRepository = require('./repository/tweet-repository');
-const Comment = require('./models/comment')
-
 const app = express();
 
 app.listen(3000,async()=>{
     console.log(`Server started at PORT 3000`);
     await connect();
     console.log('Mongo DB connected');
-    // const tweet = await Tweet.create({
-    //     content:"Second tweet",
-    // });
-    // const tweet1 = await Tweet.findById('6963cfc008eb6368b2d76e5b');
-    // console.log(typeof tweet1); 
-    // console.log(tweet);
-
-    const tweetRepo = new TweetRepository();
-    const tweet = await tweetRepo.create({userEmail:'a@b.com',content:'Tweet with hooks'});
-    console.log(tweet);
-    // const comment = await Comment.create({content:'new comment'});
-    // tweet.comments.push(comment);
-    // await tweet.save();
-    // const tweets = await tweetRepo.getAll();
-    // console.log(tweets[5].contentWithEmail);
-
 })
